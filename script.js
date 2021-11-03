2bc67908e59142ae990749488daa251526dc7cc8
Stephen Moore <judo2000@comcast.net>
2021-11-03 00:53:33 -0600

    Edited the generatePassword and other functions

--- script.js
+++ script.js
@@ -19,33 +19,44 @@ var generatePassword = function() {
   
   const numChars = prompt("How many characters (8-126)");
   if (!isNaN(numChars) && numChars >= 8 && numChars < 129) {
-    
-    const userSpecialChars = confirm("Would you like to use special characters? ($$*%_ etc)");
-    if (userSpecialChars === true) {
-      typeArr.push("specialChars");
+    const useLowerCase = confirm("Would you like to include lowercase letters?");
+    if (useLowerCase === true) {
+      typeArr.push("lowerCase");
     }
     const useUpperCase = confirm("Would you like to include uppercase letters?");
     if (useUpperCase === true) {
       typeArr.push("upperCase");
+    }
+    const userSpecialChars = confirm("Would you like to use special characters? ($$*%_ etc)");
+    if (userSpecialChars === true) {
+      typeArr.push("specialChars");
     }
     const useNumbers = confirm("Would you like to use numbers?");
     if (useNumbers === true) {
       typeArr.push("numbers");
     }
     
-    const random = Math.floor(Math.random() * myArr.length);
+    //const random = Math.floor(Math.random() * myArr.length);
     //console.log(random, myArr[random]);
     
     for (i = 1; i <= numChars; i++) {
-      const random = Math.floor(Math.random() * myArr.length);
+      const random = Math.floor(Math.random() * typeArr.length);
       //console.log(myArr[random]);
       if (typeArr[random] === "numbers") {
         myRandNum = randomNum(9);
         console.log(myRandNum);
       } else if (typeArr[random] === "upperCase") {
-       myRandUpperCase = randomLetter().toUpperCase;
+       randLetter = randomLetter();
+       myRandUpperCase = randLetter.toUpperCase();
         console.log(myRandUpperCase);
-      }
+      } else if (typeArr[random] === "lowerCase") {
+        myRandLowerCase = randomLetter();
+         console.log(myRandLowerCase);
+       } else if (typeArr[random] === "specialChars") {
+        myRandSpecialChars = randomSpecailChars();
+         console.log(myRandSpecialChars);
+       }
+       //return password;
     }
   } else {
     generatePassword();
@@ -61,4 +72,10 @@ function randomLetter() {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
+
+function randomSpecailChars() {
+  const specialChars = "!@#$%&*_-";
+
+  return specialChars[Math.floor(Math.random() * specialChars.length)];
 }
