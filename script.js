3aeaafc890c80fa8fafde84e5cccc16772f01f1f
Stephen Moore <judo2000@comcast.net>
2021-11-03 00:26:39 -0600

    Create initial generatePassword function

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+let myArr = [];
+generateBtn.addEventListener("click", writePassword);
+var generatePassword = function() { 
+  
+  const numChars = prompt("How many characters (8-126)");
+  if (!isNaN(numChars) && numChars >= 8 && numChars < 129) {
+    
+    const userSpecialChars = confirm("Would you like to use special characters? ($$*%_ etc)");
+    if (userSpecialChars === true) {
+      myArr.push("specialChars");
+    }
+    const useUpperCase = confirm("Would you like to include uppercase letters?");
+    if (useUpperCase === true) {
+      myArr.push("upperCase");
+    }
+    const useNumbers = confirm("Would you like to use numbers?");
+    if (useNumbers === true) {
+      myArr.push("numbers");
+    }
+    
+    const random = Math.floor(Math.random() * myArr.length);
+    //console.log(random, myArr[random]);
+    
+    for (i = 1; i <= numChars; i++) {
+      const random = Math.floor(Math.random() * myArr.length);
+      //console.log(myArr[random]);
+      if (myArr[random] === "numbers") {
+        myRandNum = randomNum(9);
+        console.log(myRandNum);
+      } else if (myArr[random] === "upperCase") {
+       myRandUpperCase = randomLetter().toUpperCase;
+        console.log(myRandUpperCase);
+      }
+    }
+  } else {
+    generatePassword();
+  }
+  return;
+};
