b8a62855acd850969cb6fa1949d11b8e08836418
Stephen Moore <judo2000@comcast.net>
2021-11-05 14:11:35 -0600

    Added copy to clipboard function

--- script.js
+++ script.js
@@ -99,3 +99,18 @@ function randomSpecailChars() {
   const specialChars = "!@#$%&*_-";
   return specialChars[Math.floor(Math.random() * specialChars.length)];
 }
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("password");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+  //alert("Copied the text: " + copyText.value);
+}
