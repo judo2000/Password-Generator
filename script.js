3254d6860823ce08ce53ad6f8df786081a4af8ab
Stephen Moore <judo2000@comcast.net>
2021-11-05 14:48:43 -0600

    Edit copyToClipboard function to work locally and on github

--- script.js
+++ script.js
@@ -112,10 +112,9 @@ function copyToClipboard() {
 
   /* Select the text field */
   copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
+  document.execCommand("copy");
    /* Copy the text inside the text field */
-  document.body.navigator.clipboard.writeText(copyText.value);
+  navigator.clipboard.writeText(copyText.value);
 
   /* Alert the copied text */
   //alert("Copied the text: " + copyText.value);
