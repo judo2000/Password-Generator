5c2ba70c2238d870182ce17e61923421c4a9a2bd
Stephen Moore <judo2000@comcast.net>
2021-11-05 14:14:46 -0600

    Editing coy to clipboard function

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function myFunction() {
   copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
    /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.value);
+  document.body.navigator.clipboard.writeText(copyText.value);
 
   /* Alert the copied text */
   //alert("Copied the text: " + copyText.value);
