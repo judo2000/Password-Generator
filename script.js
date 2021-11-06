18574900f42e2c23cf3eadcd95c69e41ee0adf21
Stephen Moore <judo2000@comcast.net>
2021-11-05 22:59:08 -0600

    Refactor my code

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// initialize typeArr to store character types selected by the user 
-let typeArr = [];
 
 // function to generate the password
-var generatePassword = function() { 
+let generatePassword = function() { 
+  // initialize typeArr to store character types selected by the user 
+  let typeArr = [];
   
   // prompt user for number of characters to use and store answer as numChars
   const numChars = prompt("How many characters (8-128)");
@@ -47,41 +47,45 @@ var generatePassword = function() {
       typeArr.push("numbers");
     }
     
-    // initialize passwordText string that will be created and passed to writePassword function
-    let passwordText = '';
-    // for loop to iterate numChar times
-    for (i = 1; i <= numChars; i++) {
-      // create random method to pull random type from typeArr on each iteration of for loop
-      const random = Math.floor(Math.random() * typeArr.length);
-      if (typeArr[random] === "numbers") {
-        // if random type is numbers get a random number between 0 and 9
-        myRandNum = randomNum(9);
-        // Add randomly generated number to password string
-        passwordText += myRandNum;
-      } else if (typeArr[random] === "upperCase") {
-        // if random type is upperCase call randomLetter function
-       randLetter = randomLetter();
-       // call toUpperCase method to convert random letter to an uppercase letter
-       myRandUpperCase = randLetter.toUpperCase();
-        //add randomly generated uppercase letter to password string
-        passwordText += myRandUpperCase;
-      } else if (typeArr[random] === "lowerCase") {
-        // if random type is lowerCase call randomLetter function
-        myRandLowerCase = randomLetter();
-         //add randomly generated lowercase letter to password string
-         passwordText += myRandLowerCase;
-       } else if (typeArr[random] === "specialChars") {
-         // if random type is specialChars call randomSpecialChars function
-        myRandSpecialChars = randomSpecailChars();
-         //add randomly generated lowercase letter to password string
-         passwordText += myRandSpecialChars;
-       }
-       //set passwordText to password
-       //passwordText = password;
+    // check to make sure the user has selected at least 1 type of character for the password
+    if (typeArr.length > 0) {
+      // initialize passwordText string that will be created and passed to writePassword function
+      let passwordText = '';
+      // for loop to iterate numChar times
+      for (i = 1; i <= numChars; i++) {
+        // create random method to pull random type from typeArr on each iteration of for loop
+        const random = Math.floor(Math.random() * typeArr.length);
+        if (typeArr[random] === "numbers") {
+          // if random type is numbers get a random number between 0 and 9
+          myRandNum = randomNum(9);
+          // Add randomly generated number to password string
+          passwordText += myRandNum;
+        } else if (typeArr[random] === "upperCase") {
+          // if random type is upperCase call randomLetter function
+         randLetter = randomLetter();
+         // call toUpperCase method to convert random letter to an uppercase letter
+         myRandUpperCase = randLetter.toUpperCase();
+          //add randomly generated uppercase letter to password string
+          passwordText += myRandUpperCase;
+        } else if (typeArr[random] === "lowerCase") {
+          // if random type is lowerCase call randomLetter function
+          myRandLowerCase = randomLetter();
+           //add randomly generated lowercase letter to password string
+           passwordText += myRandLowerCase;
+        } else if (typeArr[random] === "specialChars") {
+           // if random type is specialChars call randomSpecialChars function
+          myRandSpecialChars = randomSpecailChars();
+           //add randomly generated lowercase letter to password string
+           passwordText += myRandSpecialChars;
+        }
+      }
+      // return passwordText to writePassword function to be written to textarea
+      return passwordText;
       
+    // if the user does not select any character types execute the alert
+    } else {
+      alert("You must select at least one type of character to add.")
     }
-    // return passwordText to writePassword function to be written to textarea
-    return passwordText;
   } else {
     // If the user enters anything but a number between 8 and 128 alert with a message
     alert("You must enter a number between 8 and 128.");
@@ -116,5 +120,6 @@ function copyToClipboard() {
 
    /* Copy the text inside the text field */
    window.navigator.clipboard.writeText(copyText.value);
+  
 
 }
