01e63aec99f3558234f2182091d7d5cfcd8e5c15
Stephen Moore <judo2000@comcast.net>
2021-11-05 21:13:50 -0600

    Update copy to clipboard function:

--- script.js
+++ script.js
@@ -115,12 +115,6 @@ function copyToClipboard() {
   copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
    /* Copy the text inside the text field */
-   if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
-    window.navigator.clipboard.writeText(copyText.value);
-  } else {
-    window.navigator.clipboard.writeText(copyText.value);
-  }
+   window.navigator.clipboard.writeText(copyText.value);
 
-  /* Alert the copied text */
-  //alert("Copied the text: " + copyText.value);
 }
